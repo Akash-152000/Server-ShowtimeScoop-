e4df9e6d1e96a49b64df3771cecc48ddb7d3dde6
Akash <57212963+Akash-152000@users.noreply.github.com>
2023-04-21 01:29:20 +0530

    Imported dotenv file and replaced sensitive info with environment variables

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const connectToMongo = require('./db')
 const express = require('express')
+require('dotenv').config({ path: './.env' })
 
 connectToMongo();
 
 const app = express()
-const port = 5000;
+
 
 app.use(express.json())
 
@@ -12,6 +13,6 @@ app.use(express.json())
 app.use('/api/auth',require('./routes/auth'))
 
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(process.env.PORT, () => {
+  
 })
