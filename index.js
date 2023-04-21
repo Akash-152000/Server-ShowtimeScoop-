94c53f020f406738230f10533c5914cf694b84ba
Akash <57212963+Akash-152000@users.noreply.github.com>
2023-04-21 17:04:04 +0530

    import cors

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const connectToMongo = require('./db')
 const express = require('express')
 require('dotenv').config({ path: './.env' })
+const cors = require("cors");
 
 connectToMongo();
-
 const app = express()
+app.use(cors());
 
 
 app.use(express.json())
